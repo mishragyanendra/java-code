00ddf0ba87b8233061dda23769045009c0b4d792
mishragyanendra <112954307+mishragyanendra@users.noreply.github.com>
2022-09-08 21:23:22 +0530

    Add files via upload

--- Lab 08-09-2022/PrimeNumber.java
+++ Lab 08-09-2022/PrimeNumber.java
@@ -1,30 +1,37 @@
-public class PrimeNumber
-{
-    public static void main (String[]args)
-    {
-        int n = 23;
-        checkPrime(n);
-    }
+import java.util.Scanner;
 
-    private static void checkPrime(int n) {
-        int count = 0;
+class PrimeNumber {
+	
+	// checks the given number is prime is not
+	static void checkPrime(int num) {
+		boolean is_prime = true;
+		if (num == 0 || num == 1) {
+			System.out.print("Not a prime number");
+		}
+		else {
+			for (int i = 2; i <= num/2; i++) {
+				if (num % i == 0) {
+					System.out.print("Not a prime number");
+					is_prime = false;
+					break;
+				}
+			}
+			if (is_prime) {
+				System.out.print("Prime Number");
+			}
+		}
+	}
+	
+	// Main method
+	public static void main(String[] args) {
+		// creating scanner object
+		Scanner scan = new Scanner(System.in);
 
-        // negative numbers, 0 and 1 are not prime
-        if (n < 2)
-            System.out.println ("The given is number " + n + " is not prime");
+		// Creating user input
+		System.out.print("Enter any number to check number is prime or not: ");
+		int num = scan.nextInt();
+		
+		checkPrime(num);	
+	}
 
-        // checking the number of divisors b/w [1, n]
-        for (int i = 1; i <= n; i++) 
-        { 
-            if (n % i == 0) 
-                count += 1; 
-        } 
-
-        // if count of divisors greater than 2 then its not prime 
-        if (count > 2)
-            System.out.println ("The given is number " + n + " is not prime");
-
-        else
-            System.out.println ("The given is number " + n + " is prime");
-    }
 }
